8cc1aa88bcf507c647726dbcc0260a6e8c0f2ebe
Brandbil <Abbeerik@gmail.com>
2022-11-04 20:58:58 +0100

    added genetalia

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.text(55, 41, "Hello, snåp!", pyxel.frame_count % 16)
         pyxel.blt(61, 66, 0, 0, 0, 38, 16)
 
 App()
